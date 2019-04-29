d1fd1ee4bdc505f132fe0585c68c09fc688eef50
Deeathwiing <andreykorotkins@gmail.com>
2019-04-29 11:16:14 +0300

    fix

--- src/Components/main/section/administration/allItemsAdmin.jsx
+++ src/Components/main/section/administration/allItemsAdmin.jsx
@@ -1,29 +1,34 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const AllItemsAdmin = props => (
-  <div className="card-columns">
-    {props.state.map(item => (
-      <div key={item.id} className="card" data-id={item.id}>
-        <img src={item.picture} className="card-img-top" alt={item.title} />
-        <div className="card-body">
-          <h2 className="card-title">{item.title}</h2>
-          <p className="card-text">{item.description}</p>
-          <p className="card-text">{item.price}</p>
-          <p className="card-text">{item.tags}</p>
-          <button
-            type="button"
-            className="btn-danger btn-block btnForDelete"
-            data-id={item.id}
-            onClick={props.deleteItems}
-          >
-            Delete item
-          </button>
-        </div>
+const AllItemsAdmin = (props) => {
+  const lastElement = props.state[props.state.length - 1];
+  if (lastElement) {
+    return (
+      <div className="card-columns">
+        {props.state.map(item => (
+          <div key={item.id} className="card" data-id={item.id}>
+            <img src={item.picture} className="card-img-top" alt={item.title} />
+            <div className="card-body">
+              <h2 className="card-title">{item.title}</h2>
+              <p className="card-text">{item.description}</p>
+              <p className="card-text">{item.price}</p>
+              <p className="card-text">{item.tags}</p>
+              <button
+                type="button"
+                className="btn-danger btn-block btnForDelete"
+                data-id={item.id}
+                onClick={props.deleteItems}
+              >
+                Delete item
+              </button>
+            </div>
+          </div>
+        ))}
       </div>
-    ))}
-  </div>
-);
+    );
+  } return null;
+};
 
 export default AllItemsAdmin;
 
